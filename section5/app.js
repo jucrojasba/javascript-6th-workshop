ae077f2dd1b137641cb1874ad92af08882a8bc86
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-05-01 22:14:38 -0500

    Add functions of validate information for booking, complete cargarYMostrar.then() in section 5

--- section5/app.js
+++ section5/app.js
@@ -1,13 +1,44 @@
 // Ruta del archivo data.json
 const url = "data.json";
+// Reservaciones
+let bookings =[];
 
-//Función para validar que el valor ingresado por el ususario sea de 3 digitos y sea un valor numerico.
+//Función para validar que el valor ingresado por el ususario sea un valor numerico.
 function validateNumber(value){
-  const validateNumber = /^\d{3}$/;
+  const validateNumber = /^\d+$/;
   if (validateNumber.test(value)) {
     return true;
   } else {
-    alert(`Opción inválida, porfavor ingresa el numero de la habitación que deseas reservar`);
+    alert(`Opción inválida, porfavor ingresa números`);
+    return false;
+  }
+}
+
+//Funcion para validar que el nombre ingresado por el usuario solo tenga su primer nombre y su primer apellido
+function validateGuestName(value){
+  const validateWord = /^[a-zñáéíóú]+ [a-zñáéíóú]+$/i;
+  if (validateWord.test(value)) {
+    return  true;
+  } else {
+    alert(`Valor inválido, ingresa tu primer nombre y tu primer apellido`);
+  }
+}
+
+//Funcion para validar que la fecha sea despues de hoy() y que este ingresada en formato DD/MM/AAAA
+function validateDate(value){
+  const validateDate = /^\d{2}\/\d{2}\/\d{4}$/;
+  const hoy = new Date();
+  if (validateDate.test(value)) {
+    const [dia, mes, año] = value.split('/');
+    const fechaUsuario = new Date(`${año}-${mes}-${dia}`);
+    if (fechaUsuario.getTime() > hoy.getTime()) {
+      return true;
+    } else {
+      alert(`La fecha no puede ser antes de hoy, porfavor ingresa una fecha correcta`);
+      return false;
+    }
+  } else {
+    alert(`Formato inválido, ingresa porfavor una fecha con el siguiente formato: DD/MM/AAAA`)
     return false;
   }
 }
@@ -39,43 +70,71 @@ function cargarYMostrarData() {
 }
 
 // Función para registrar reservas
-function crearReserva(numeroHabitacion, fechaInicio, fechaFin, huesped) {
-  function generarGeneradorId() {
-    let id = 1; // Variable id se inicializa fuera de la función interna
+// function crearReserva(numeroHabitacion, fechaInicio, fechaFin, huesped) {
+  
+//   function generarGeneradorId() {
+//     let id = 1; // Variable id se inicializa fuera de la función interna
 
-    return function () {
-      return id++; // Cada vez que se llama a la función, se incrementa id y se devuelve
-    };
-  }
+//     return function () {
+//       return id++; // Cada vez que se llama a la función, se incrementa id y se devuelve
+//     };
+//   }
 
-  const generarId = generarGeneradorId(); // Se obtiene la función interna generarId()
+//   const generarId = generarGeneradorId(); // Se obtiene la función interna generarId()
 
-  // Pruebas
-  console.log(generarId()); // 1
-  console.log(generarId()); // 2
-  console.log(generarId()); // 3
-  console.log(generarId()); // 4
-  console.log(generarId()); // 5
-}
+// }
 
 // Llamar a la función para cargar y mostrar el contenido de data.json
 cargarYMostrarData()
   .then(({ rooms, roomTypes }) => {
     // Mostrar el contenido de las habitaciones después de cargar los datos
-    let userInput;
+    let numeroHabitacion, cantidadHuespedes, fechaInicio, fechaFin, huesped;
     do {
-      userInput = prompt(
-        "Ingrese el numero de habitacion a reservar: " +
+      cantidadHuespedes = parseFloat(prompt(`Te damos la bienvenida! \n\nPara iniciar tu proceso de reserva cuentanos:\n\n¿cuantos huespedes se quedaran en la misma habitación? (máx 6) \n\nEscribe profavor un número:`));
+      
+    } while (!validateNumber(cantidadHuespedes) || cantidadHuespedes>6);
+    let roomNumberAvailable = rooms.map((room) => {
+      if(room.availability && parseFloat(roomTypes.find((type) => type.id === room.roomTypeId).capacity) >= cantidadHuespedes){
+        return room.number
+      };
+      });
+    do {
+      numeroHabitacion = parseFloat(prompt(
+        "Las habitaciones disponibles para esa cantidad de personas son:" +
           rooms
             .map((room) => {
-              return `\nRoom Number: ${room.number} (${
-                roomTypes.find((type) => type.id === room.roomTypeId).name
-              })`;
+              if(room.availability && parseFloat(roomTypes.find((type) => type.id === room.roomTypeId).capacity) >= cantidadHuespedes){
+                return `\nRoom Number: ${room.number} (${
+                  roomTypes.find((type) => type.id === room.roomTypeId).name
+                })`;
+              }
             })
-            .join(" ")
-      );
-    } while (!validateNumber(userInput.trim()));
-    return userInput;
+            .join(" ")+"\n\nIngrese el numero de habitacion a reservar:" 
+      ));
+      if(!roomNumberAvailable.includes(numeroHabitacion)){
+        alert(`Esa habitación no se encuentra en la lista proporcionada, porfavor ingresa un numero de habiatción válido`)
+      }
+    } while (!validateNumber(numeroHabitacion) || !(roomNumberAvailable.includes(numeroHabitacion)));
+
+    do {
+      fechaInicio = prompt("Ingresa la fecha en la que nos visitaras (check in): \n\nEscribela porfavor en formato DD/MM/AAAA")
+    } while (!validateDate(fechaInicio));
+    let dateFin, dateInicio;
+    do {
+      fechaFin = prompt("Ingresa la fecha en la que saldrás (check out): \n\nEscribela porfavor en formato DD/MM/AAAA");
+      const [diaFin, mesFin, añoFin] = fechaFin.split('/');
+      const [diaInicio, mesInicio, añoInicio] = fechaInicio.split('/');
+      
+      dateFin = new Date(`${añoFin}-${mesFin}-${diaFin}`);
+      dateInicio = new Date(`${añoInicio}-${mesInicio}-${diaInicio}`);
+      if((dateFin <= dateInicio)){
+      alert(`La fecha de check out debe ser posterior a la fecha del check in`);
+      }
+    } while (!validateDate(fechaFin) || (dateFin <= dateInicio));
+    do {
+      huesped = prompt("Ingresa el primer nombre y el primer apellido de quien reserva de la reserva").trim().toLowerCase();
+    } while (!validateGuestName(huesped));
+    return numeroHabitacion, cantidadHuespedes, fechaInicio, fechaFin, huesped, rooms,roomTypes;
   })
   .catch((error) => {
     console.error("Error al manejar la promesa:", error);
