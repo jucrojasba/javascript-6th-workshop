dcc53225e397275bba14be578579edc2c5d69cbc
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-05-02 21:29:27 -0500

    Add editbookigs() in section 5

--- section5/app.js
+++ section5/app.js
@@ -1,9 +1,10 @@
 // Ruta del archivo data.json
 const url = "data.json";
 // Reservaciones
-let bookings = [];
+let bookings = [],
+id = 1;
 
-//Función para validar que el valor ingresado por el ususario sea un valor numerico.
+//Función para validar que el valor ingresado por el ususario sea un valor numerico
 function validateNumber(value) {
   const validateNumber = /^\d+$/;
   if (validateNumber.test(value)) {
@@ -73,6 +74,13 @@ function cargarYMostrarData() {
   });
 }
 
+//Funcion generación de Id
+function generarGeneradorId() {
+  return function () {
+    return id++; // Cada vez que se llama a la función, se incrementa id y se devuelve
+  };
+}
+
 // Función para registrar reservas
 function crearReserva(a, b) {
   let numeroHabitacion,
@@ -81,12 +89,18 @@ function crearReserva(a, b) {
     fechaFin,
     huesped,
     rooms = a,
-    roomTypes = b;
+    roomTypes = b,
+    flag=true;
   validateCantidadHuespedes();
   validateHabitacion();
-  checkin();
+  if(flag){
+    checkin();
   checkout();
+  validateGuest();
   confirmationBooking();
+  }else{
+    menu();
+  }
   function validateCantidadHuespedes() {
     do {
       cantidadHuespedes = parseFloat(
@@ -131,13 +145,13 @@ function crearReserva(a, b) {
       );
       if (!roomNumberAvailable.includes(numeroHabitacion)) {
         alert(
-          `Esa habitación no se encuentra en la lista proporcionada, porfavor ingresa un numero de habiatción válido`
+          `Esa habitación no se encuentra en la lista proporcionada, vuelve a intentarlo`
         );
+        flag = false;
       }
     } while (
-      !validateNumber(numeroHabitacion) ||
-      !roomNumberAvailable.includes(numeroHabitacion)
-    );
+      !validateNumber(numeroHabitacion));
+    
   }
   function checkin() {
     do {
@@ -163,6 +177,8 @@ function crearReserva(a, b) {
         );
       }
     } while (!validateDate(fechaFin) || dateFin <= dateInicio);
+  }
+  function validateGuest(){
     do {
       huesped = prompt(
         "Ingresa el primer nombre y el primer apellido de quien reserva de la reserva"
@@ -170,13 +186,6 @@ function crearReserva(a, b) {
         .trim()
         .toLowerCase();
     } while (!validateGuestName(huesped));
-  }
-  function generarGeneradorId() {
-    let id = 1; // Variable id se inicializa fuera de la función interna
-
-    return function () {
-      return id++; // Cada vez que se llama a la función, se incrementa id y se devuelve
-    };
   }
   function confirmationBooking() {
     let confirmBooking = confirm(
@@ -240,7 +249,8 @@ function verReservas() {
 }
 
 //Funcion para cancelar reservar
-function cancelarReservas() {
+function cancelarReservas(a) {
+  let rooms = a;
   if (bookings.length != 0) {
     let huesped;
     do {
@@ -263,20 +273,93 @@ function cancelarReservas() {
       let userId = parseFloat(
         prompt("Ingresa el id de la reservaciòn que deseas cancelar").trim()
       );
-      if (bookings.some((e) => e.id == userId)) {
-        huesped = prompt(
-          "Para confirmar ingresa nuevamente el primer nombre y el primer apellido de la persona que hizo la reservaciòn"
-        ).trim();
-        if (bookings.some((e) => e.id == userId && e.huesped == huesped)) {
+      if (bookings.some((e) => e.id == userId && e.huesped == huesped)) {
+          let numeroHabitacion = bookings.find(e=>e.id == userId).numeroHabitacion; 
+          rooms.forEach((element) => {
+            if (parseFloat(element.number) == numeroHabitacion) {
+              element.availability = true;
+            }});
           bookings = bookings.filter((e) => e.id != userId);
           alert(
             `La reserva a nombre de ${huesped} con id: ${userId} ha sido eliminada con èxito`
           );
-        } else {
+          return rooms;
+      } else {
+        alert(
+          `El Id ingresado no ha sido asignado a ninguna reserva, vuelve a intentarlo. \n\nTe redigiremos al menu`
+        );
+      }
+    } else {
+      alert(`No se encontrò ninguna reserva a nombre de ${huesped}`);
+    }
+  } else {
+    alert(`No existen reservas agregadas en nuestra base de datos`);
+  }
+}
+
+//Funcion editar reservas
+function editBooking() {
+  if (bookings.length != 0) {
+    let huesped, 
+    fechaInicio,
+    fechaFin;
+    do {
+      huesped = prompt(
+        "Ingresa el primer nombre y primer apellido de la persona que hizo la reserva"
+      )
+        .trim()
+        .toLowerCase();
+    } while (!validateGuestName(huesped));
+    if (bookings.some((e) => e.huesped == huesped)) {
+      alert(
+        `Las siguientes reservas fueron encontradas a nombre de ${huesped}:\n` +
+          bookings
+            .filter((e) => e.huesped == huesped)
+            .map(
+              (e) =>
+                `\n- id(${e.id}) Habitaciòn reservada: ${e.numeroHabitacion} para las fechas ${e.fechaInicio} a ${e.fechaFin}`
+            )
+      );
+      let userId = parseFloat(
+        prompt("Ingresa el id de la reservaciòn que deseas editar").trim()
+      );
+      if (bookings.some((e) => e.id == userId && e.huesped == huesped)) {
+          let numeroHabitacion = bookings.find(e=>e.id == userId).numeroHabitacion; 
+          function checkin() {
+            do {
+              fechaInicio = prompt(
+                "Ingresa la fecha en la que nos visitaras (check in): \n\nEscribela porfavor en formato DD/MM/AAAA"
+              );
+            } while (!validateDate(fechaInicio));
+          }
+          function checkout() {
+            let dateFin, dateInicio;
+            do {
+              fechaFin = prompt(
+                "Ingresa la fecha en la que saldrás (check out): \n\nEscribela porfavor en formato DD/MM/AAAA"
+              );
+              const [diaFin, mesFin, añoFin] = fechaFin.split("/");
+              const [diaInicio, mesInicio, añoInicio] = fechaInicio.split("/");
+        
+              dateFin = new Date(`${añoFin}-${mesFin}-${diaFin}`);
+              dateInicio = new Date(`${añoInicio}-${mesInicio}-${diaInicio}`);
+              if (dateFin <= dateInicio) {
+                alert(
+                  `La fecha de check out debe ser posterior a la fecha del check in`
+                );
+              }
+            } while (!validateDate(fechaFin) || dateFin <= dateInicio);
+          }
+          checkin();
+          checkout();
+          bookings.forEach((element) => {
+            if (parseFloat(element.number) == numeroHabitacion) {
+              element.fechaInicio=fechaInicio;
+              element.fechaFin = fechaFin;
+            }});
           alert(
-            `Lo sentimos el nombre ingresado no corresponde con la persona que realizo la reservaciòn`
+            `La reserva a nombre de ${huesped} con id: ${userId} ha sido actualizada con èxito`
           );
-        }
       } else {
         alert(
           `El Id ingresado no ha sido asignado a ninguna reserva, vuelve a intentarlo. \n\nTe redigiremos al menu`
@@ -314,7 +397,10 @@ function menu() {
             verReservas();
             break;
           case "3":
-            cancelarReservas();
+            cancelarReservas(rooms);
+            break;
+          case "4":
+            editBooking(rooms);
             break;
           case "5":
             flag = false;
